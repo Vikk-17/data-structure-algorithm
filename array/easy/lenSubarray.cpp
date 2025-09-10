57d08b3acca66c0e76635a2ef55c38cff10e1788
Souvik Chakraborty <Souvikc3030@gmail.com>
2025-09-10 18:01:48 +0530

    lenSubarrayOptimal

--- array/easy/lenSubarray.cpp
+++ array/easy/lenSubarray.cpp
@@ -22,13 +22,30 @@ int getLongestSubarray(vector<int>& a, long long k) {
     return len;
 }
 
+int getLongestSubarrayOptimal(vector<int>& nums, long long k) {
+    int left = 0, right = 0;
+    int size = nums.size();
+    long long sum = nums[0];
+    int maxLen = 0;
+    while(right < size) {
+        while(left <= right && sum > k) {
+            sum -= nums[left];
+            left ++;
+        }
+        if(sum == k) maxLen = max(maxLen, right - left + 1);
+        right ++;
+        if(right < size) sum += nums[right]; 
+    }
+    return maxLen;
+}
+
 int main()
 {
     vector<int> a = {2, 3, 5, 1, 9};
-    vector<int> b = {1, 2, 1, 2, 1};
-    // long long k = 10;
-    long long k = 3;
-    int len = getLongestSubarray(b, k);
+    // vector<int> b = {1, 2, 1, 2, 1};
+    long long k = 10;
+    // long long k = 3;
+    int len = getLongestSubarrayOptimal(a, k);
     cout << "The length of the longest subarray is: " << len << "\n";
     return 0;
 }
